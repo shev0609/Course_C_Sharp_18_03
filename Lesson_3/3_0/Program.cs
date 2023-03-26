144602a92b8a2da2954fd316a4cc61b0813fcbd9
shev0609 <m.lolita653@gmail.com>
2023-03-26 12:46:16 +0500

    Lesson_2

--- Lesson_3/3_0/Program.cs
+++ Lesson_3/3_0/Program.cs
@@ -2,16 +2,16 @@
 
 void Quarters(int x, int y)
 {
-if (x == 0 || y == 0)
-Console.WriteLine("x = 0, y = 0");
-else if (x > 0 && y > 0)
-Console.WriteLine("I");
-else if (x < 0 && y > 0)
-Console.WriteLine("II");
-else if (x < 0 && y < 0)
-Console.WriteLine("III");
-else if (x > 0 && y < 0)
-Console.WriteLine("IV");
+    if (x == 0 || y == 0)
+        Console.WriteLine("x = 0, y = 0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("I");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
 }
 int num_1 = int.Parse(Console.ReadLine()!);
 int num_2 = int.Parse(Console.ReadLine()!);
