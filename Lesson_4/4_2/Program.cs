c7a9220ae2076e106df8519790a3422506ca76aa
shev0609 <m.lolita653@gmail.com>
2023-03-26 14:23:46 +0500

    4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int protectnomber(int num)
+{
+    int all_pr = 1;
+    for (int i = 1; i <= num; i++)
+        all_pr *= i;
+
+    return all_pr;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = protectnomber(A);
+Console.WriteLine(result);
